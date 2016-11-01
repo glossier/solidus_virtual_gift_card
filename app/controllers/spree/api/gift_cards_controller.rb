9756805e6671a20a44b365e43703a000ae4380ed
David Ang <davidang09@gmail.com>
2016-11-02 04:44:19 +0800

    add additional api endpoint that checks for existing of unused redemption code

--- app/controllers/spree/api/gift_cards_controller.rb
+++ app/controllers/spree/api/gift_cards_controller.rb
@@ -1,7 +1,6 @@
 class Spree::Api::GiftCardsController < Spree::Api::BaseController
 
   def redeem
-    redemption_code = Spree::RedemptionCodeGenerator.format_redemption_code_for_lookup(params[:redemption_code] || "")
     @gift_card = Spree::VirtualGiftCard.active_by_redemption_code(redemption_code)
 
     if !@gift_card
@@ -13,11 +12,23 @@ class Spree::Api::GiftCardsController < Spree::Api::BaseController
     end
   end
 
+  def code_exists
+    if Spree::VirtualGiftCard.active_by_redemption_code(params[:redemption_code])
+      head :found
+    else
+      head :not_found
+    end
+  end
+
   private
 
   def redeem_fail_response
    {
       error_message: "#{Spree.t('admin.gift_cards.errors.not_found')}. #{Spree.t('admin.gift_cards.errors.please_try_again')}"
    }
- end
+  end
+
+  def redemption_code
+    Spree::RedemptionCodeGenerator.format_redemption_code_for_lookup(params[:redemption_code] || "")
+  end
 end
