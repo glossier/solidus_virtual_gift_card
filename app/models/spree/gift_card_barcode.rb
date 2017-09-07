1e6ad3b9ec604f626f41769730cf834252d96dc1
David Ang <davidang09@gmail.com>
2017-09-08 01:23:35 +0800

    the gift card barcode (physical gift card)

---
+++ app/models/spree/gift_card_barcode.rb
@@ -0,0 +1,48 @@
+module Spree
+  class GiftCardBarcode < Spree::Base
+    acts_as_paranoid
+    validates_uniqueness_of :number, unless: :deleted_at
+
+    belongs_to :virtual_gift_card, class_name: 'Spree::VirtualGiftCard'
+    belongs_to :order, class_name: 'Spree::Order'
+
+    scope :unredeemed, -> { where(redeemed_at: nil) }
+
+    self.whitelisted_ransackable_attributes = %w[number]
+
+    def sold!(gift_card, order)
+      self.order = order
+      self.virtual_gift_card = gift_card
+      self.sold_at = Time.now
+
+      save
+    end
+
+    def redeem(user)
+      return if redeemed?
+      user.store_credits.create!(amount: amount, category: store_credit_category, currency: 'USD', created_by: user)
+      redeem_associated_gift_card
+      update_attributes(redeemed_at: Time.now)
+    end
+
+    def store_credit_category
+      Spree::StoreCreditCategory.gift_card
+    end
+    
+
+    def redeemed?
+      redeemed_at.present?
+    end
+
+    def self.active(number)
+      Spree::GiftCardBarcode.unredeemed.find_by(number: number)
+    end
+
+    private
+
+    def redeem_associated_gift_card
+      return if virtual_gift_card.nil?
+      virtual_gift_card.update_attributes(redeemed_at: Time.now)
+    end
+  end
+end
