87f2394c3f887ce38baa0f7387788676ea2e7aba
David Ang <davidang09@gmail.com>
2017-09-08 01:23:52 +0800

    convenience method to pull the gift card category from the store credits category

--- app/models/spree/store_credit_category_decorator.rb
+++ app/models/spree/store_credit_category_decorator.rb
@@ -1,2 +1,20 @@
+module Spree
+  module StoreCreditCategoryExtensions
+    module ClassMethods
+      def gift_card
+        Spree::StoreCreditCategory.find_or_create_by(name: Spree::StoreCreditCategory::GIFT_CARD_CATEGORY_NAME)
+      end
+    end
+    
+    def self.prepended(base)
+      class << base
+        prepend ClassMethods
+      end
+    end
+  end
+end
+
+Spree::StoreCreditCategory.prepend Spree::StoreCreditCategoryExtensions
+
 Spree::StoreCreditCategory::GIFT_CARD_CATEGORY_NAME = "Gift Card".freeze
 Spree::StoreCreditCategory.non_expiring_credit_types |= [Spree::StoreCreditCategory::GIFT_CARD_CATEGORY_NAME]
